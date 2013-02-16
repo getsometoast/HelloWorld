5dfd4c8960fa5f4217efc56c610315b969892e86
getsometoast <james@jameslewiscv.com>
2013-02-16 12:57:11 +0000

    initial commit, added EVERYTHING

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jameslewis
+ * Date: 16/02/2013
+ * Time: 12:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
